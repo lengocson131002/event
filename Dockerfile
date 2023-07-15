8fdbe36459938c4d3e6e72bb82cb0593defb48be
lengocson <leson131002@gmail.com>
2023-07-15 23:29:27 +0700

    fix font

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,6 @@ ARG JAR_FILE=build/libs/EventManagement-0.0.1-SNAPSHOT.jar
 COPY ${JAR_FILE} app.jar
 EXPOSE 8080
 
+RUN apk add --no-cache fontconfig ttf-dejavu
+
 ENTRYPOINT ["java","-Duser.timezone=UTC","-jar","/app.jar"]
--- src/main/java/com/app/event/util/ExcelExporter.java
+++ src/main/java/com/app/event/util/ExcelExporter.java
@@ -2,8 +2,12 @@ package com.app.event.util;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayInputStream;
@@ -60,9 +64,15 @@ public class ExcelExporter {
 
     private void createHeaderRow(Sheet sheet, String[] columnNames) {
         Row headerRow = sheet.createRow(0);
+        CellStyle headerCellStyle = sheet.getWorkbook().createCellStyle();
+        HSSFFont headerFont = (HSSFFont)sheet.getWorkbook().createFont();
+        headerFont.setBold(true);
+        headerCellStyle.setFont(headerFont);
+
         for (int i = 0; i < columnNames.length; i++) {
             Cell cell = headerRow.createCell(i);
             cell.setCellValue(columnNames[i]);
+            cell.setCellStyle(headerCellStyle);
         }
     }
 
